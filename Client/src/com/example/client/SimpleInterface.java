a172f2e43dd67097e8ca800fbfb8f41dadcdd474
snzt <ryota3988@gmail.com>
2015-02-04 05:07:28 +0900

    hogehoge

---
+++ Client/src/com/example/client/SimpleInterface.java
@@ -0,0 +1,11 @@
+package com.example.client;
+
+import org.alljoyn.bus.BusException;
+import org.alljoyn.bus.annotation.BusInterface;
+import org.alljoyn.bus.annotation.BusMethod;
+
+@BusInterface(name="org.alljoyn.bus.samples.simple.SimpleInterface")
+public interface SimpleInterface {
+	@BusMethod
+	String Ping(String inStr) throws BusException;
+}
